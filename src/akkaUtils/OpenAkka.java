50537f3107a2247f78c9e5a79ee34d99a4439657
NightFish <nightfish.net@gmail.com>
2020-06-04 19:45:08 +1200

    Fix port number command line arguments

--- src/akkaUtils/OpenAkka.java
+++ src/akkaUtils/OpenAkka.java
@@ -4,8 +4,14 @@ import java.util.Collections;
 import akka.actor.ActorSystem;
 public class OpenAkka{
   public static void main(String[]args) throws InterruptedException {
-    ActorSystem s = AkkaConfig.newSystem("OpenAkka",Integer.parseInt(args[0]),Collections.emptyMap());
-    System.out.println("Akka running on port: " + args[0]);
+    int port = 2500;
+    if(args.length >= 1) {
+      try {
+        port = Integer.parseInt(args[0]);
+      } catch (Exception e) {}
+    }
+    ActorSystem s = AkkaConfig.newSystem("OpenAkka", port, Collections.emptyMap());
+    System.out.println("Akka running on port: " + port);
     AkkaConfig.keybordClose(s);
   }
 }
